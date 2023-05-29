9c9be2f91ca902464844f0dba10ce216e348c3d5
davidRamirez1005 <ramirezjd1005@gmail.com>
2023-05-29 16:54:45 -0500

    chore: :wrench: root
    creacion del documento root

---
+++ index.php
@@ -0,0 +1,43 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>php</title>
+</head>
+<body>
+
+
+<?php
+$variable = "jose david";
+var_dump($variable);
+echo "<br>"."<br>";
+echo $variable;
+// echo phpinfo();
+echo "<br>"."<br>";
+$text = "Hola";
+print $text;
+echo "<br>"."<br>";
+
+// header("Content-Type: application/pdf");
+
+define("MENSAJE","mundo");
+
+echo $text." ".MENSAJE."<br>";
+
+var_dump(MENSAJE);
+
+
+
+
+
+
+
+
+
+
+
+?>
+</body>
+</html>
