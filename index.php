9bfcdc778032fa7867e02944c8ede6a861d37b05
davidRamirez1005 <ramirezjd1005@gmail.com>
2023-06-05 17:28:53 -0500

    feat: :sparkles: aprendizaje
    cosas curiosas de php

--- index.php
+++ index.php
@@ -292,7 +292,7 @@ $frutas = array("manzana", "banana", "cereza");
 foreach ($frutas as $indice => $fruta) {
     echo "En la posición " . $indice . " se encuentra la fruta: " . $fruta . "<br>";
 }
-echo "<hr>"
+echo "<hr>";
 /**
  *  ? estructuras de control = funcionan igual que en js para tomar decisiones
  */
@@ -312,8 +312,28 @@ echo "<hr>"
 // do{
 
 // }while(false);
+echo "<textarea name='misdatos'>\n";
+echo htmlspecialchars($datos)."\n";
+echo "</textarea>";
 
+echo '<input type="text" value="' . htmlspecialchars($datos) . '" />'."\n";
 
+echo "<hr>";
+
+$contrasena = 'contrasena123';
+$hash = password_hash($contrasena, PASSWORD_DEFAULT);
+echo $hash;
+
+$contrasena_ingresada = 'contrasena123';
+echo "<hr>";
+
+if (password_verify($contrasena_ingresada, $hash)) {
+    echo 'La contraseña es correcta';
+} else {
+    echo 'La contraseña es incorrecta';
+};
+
+echo "<hr>";
 
 
 
