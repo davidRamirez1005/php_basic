4dadba887432f47e24aed134c084043062aaac62
davidRamirez1005 <ramirezjd1005@gmail.com>
2023-06-07 16:23:36 -0500

    feat: :sparkles: file get contents
    hcaer post y get

---
+++ api.php
@@ -0,0 +1,16 @@
+<?php
+    $credenciales["http"]["method"] = "POST";
+    $credenciales["http"]["header"] = "Content-type: application/json";
+    $data = [
+        "cc"=>"123",
+        "nombre"=> "jose",
+        "apellido"=> "ramirez",
+        "edad"=> 19
+    ];
+    $data = json_encode($data);
+    $credenciales["http"]["content"] = $data;
+    $config = stream_context_create($credenciales);
+
+    $_DATA = file_get_contents("https://6480ebf7f061e6ec4d4a1664.mockapi.io/informacion", false, $config);
+    print_r($_DATA);
+?>  
