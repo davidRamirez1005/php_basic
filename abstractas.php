73f504efadc8a398438800eecb230a913e039a7d
davidRamirez1005 <ramirezjd1005@gmail.com>
2023-06-14 17:31:05 -0500

    feat: :sparkles: funcionalidad del ejercicio
    usando clase abstracta y los requerimientos

--- abstractas.php
+++ abstractas.php
@@ -1,3 +1,4 @@
+<?php
 /*
 Desafío: Crea una clase abstracta llamada CuentaBancaria con los siguientes atributos y métodos:
 
@@ -14,8 +15,47 @@ La clase CuentaCorriente debe permitir retiros incluso si el saldo es insuficien
 
 La clase CuentaAhorros debe permitir retiros solo si el saldo es suficiente y no aplicar ningún cargo adicional.
 */
-<?php
 
+abstract class CuentaBancaria{
+    public function __construct(public string $saldo){}
+    public function depositar($monto){
+    }
+    public function retirar($monto){
+
+    }
+}
+
+class CuentaCorriente extends CuentaBancaria{
+    public function depositar($monto){
+        return $monto + $this -> saldo;
+    }
+    public function retirar($monto){
+        $resta = ($monto > $this -> saldo) ? $monto - $this -> saldo : ($monto - $this -> saldo)*0.10;
+        return $resta;
+    }
+}
+class CuentaAhorros extends CuentaBancaria{
+    public function depositar($monto){
+        return $monto + $this -> saldo;
+    }
+    public function retirar($monto){
+        $resta = ($monto > $this -> saldo) ? $monto - $this -> saldo : "no hay monto suficiente para retirar";
+        return $resta;
+    }
+}
+$monto = 900;
+$obj = new CuentaCorriente(1000);
+print "<br>";
+print $obj->depositar($monto);
+print "<br>";
+print $obj->retirar($monto);
+print "<hr>";
+
+$obj = new CuentaAhorros(1000);
+print "<br>";
+echo $obj->depositar($monto);
+echo "<br>";
+echo $obj->retirar($monto);
 
 
 
